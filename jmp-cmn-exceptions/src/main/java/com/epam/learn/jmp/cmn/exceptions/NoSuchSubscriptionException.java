0120afb1ae35934bb2b5df7521a871b763437da0
Alex John Wright <alex_thambiraj@epam.com>
2024-03-29 15:18:30 +0530

    Added java doc for custom exception

--- jmp-cmn-exceptions/src/main/java/com/epam/learn/jmp/cmn/exceptions/NoSuchSubscriptionException.java
+++ jmp-cmn-exceptions/src/main/java/com/epam/learn/jmp/cmn/exceptions/NoSuchSubscriptionException.java
@@ -5,13 +5,32 @@ package com.epam.learn.jmp.cmn.exceptions;
  */
 public class NoSuchSubscriptionException extends Exception{
 
+    /**
+     *
+     * Holder to store error message for the exception
+     *
+     */
     String message;
 
+    /**
+     *
+     * get the error message of the exception
+     *
+     * @return
+     * String error message of the exception
+     */
     @Override
     public String getMessage() {
         return message;
     }
 
+    /**
+     *
+     * Set the error message of the exception
+     *
+     * @param message
+     * error message
+     */
     public void setMessage(String message) {
         this.message = message;
     }
